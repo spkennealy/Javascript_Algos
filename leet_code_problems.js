b9f289841f76b839435d8dce882f00888f4bfa51
Sean Kennealy <spkennealy@gmail.com>
2019-07-03 08:35:31 -0700

    #14 Longest Common Prefix

--- leet_code_problems.js
+++ leet_code_problems.js
@@ -379,8 +379,41 @@ var romanToInt = function (s) {
     return converted;
 };
 
-console.log(romanToInt("III")); // 3
-console.log(romanToInt("IV")); // 4
-console.log(romanToInt("IX")); // 9
-console.log(romanToInt("LVIII")); // 58
-console.log(romanToInt("MCMXCIV")); // 1994
+// console.log(romanToInt("III")); // 3
+// console.log(romanToInt("IV")); // 4
+// console.log(romanToInt("IX")); // 9
+// console.log(romanToInt("LVIII")); // 58
+// console.log(romanToInt("MCMXCIV")); // 1994
+
+
+
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+
+
+
+// 14. LONGEST COMMON PREFIX
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: ["flower", "flow", "flight"]
+// Output: "fl"
+
+// Example 2:
+
+// Input: ["dog", "racecar", "car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// Note:
+// All given inputs are in lowercase letters a - z.
+
+var longestCommonPrefix = function (strs) {
+
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // "fl"
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); // ""
